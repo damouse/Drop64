8f28e0bdc5e22134fc86698789c0df3120fb6980
damouse <mickey.barboi@gmail.com>
2017-02-15 13:38:20 -0600

    fixes to  touches for smartphones

--- src/controller.js
+++ src/controller.js
@@ -1,16 +1,43 @@
 angular.module('controller', [])
-  .controller('RootController', function($scope, $location) {
-    var socket = io.connect($location.$$host + ':' + $location.$$port);
+  .directive('myTouchstart', [function() {
+    return function(scope, element, attr) {
 
-    $scope.press = function(b, k) {
-      socket.emit("controller", { event: 'keydown', button: b, code: k });
+      element.on('touchstart', function(event) {
+        scope.$apply(function() {
+          scope.$eval(attr.myTouchstart);
+        });
+      });
     };
+  }]).directive('myTouchend', [function() {
+    return function(scope, element, attr) {
 
-    // Touch stopped
-    $scope.release = function(b, k) {
-      socket.emit("controller", { event: 'keyup', button: b, code: k });
+      element.on('touchend', function(event) {
+        scope.$apply(function() {
+          scope.$eval(attr.myTouchend);
+        });
+      });
     };
-  })
+  }])
+
+.controller('RootController', function($scope, $location) {
+  var socket = io.connect($location.$$host + ':' + $location.$$port);
+  $scope.counter = 0;
+
+  $scope.touchesStarted = function() {
+    console.log("Touch Started");
+    $scope.counter++;
+  }
+
+
+  $scope.press = function(b, k) {
+    socket.emit("controller", { event: 'keydown', button: b, code: k });
+  };
+
+  // Touch stopped
+  $scope.release = function(b, k) {
+    socket.emit("controller", { event: 'keyup', button: b, code: k });
+  };
+})
 
 /**
  * Add handlers to dpad-buttons
